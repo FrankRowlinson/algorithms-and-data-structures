caa257ab171f8b22e4a48eb021b50b9464c5e676
rowlinson <johk2293@gmail.com>
2022-07-20 15:17:54 +0400

    add fibonacci generator

--- largest_increasing_subsequence.py
+++ largest_increasing_subsequence.py
@@ -57,6 +57,15 @@ def lnis_bottom_up(array: list) -> int:
     return answer, lnis
 
 
+def lis(A: list) -> int:
+    F = [0] * len(A)
+    for i in range(len(A)):
+        for j in range(i):
+            if A[j] < A[i] and F[j] > F[i]:
+                F[i] = F[j]
+        F[i] += 1
+
+
 
 
 
